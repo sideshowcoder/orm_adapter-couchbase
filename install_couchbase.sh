fef096b1e71eb8858ff09172e411df975ee9d469
Philipp Fehre <philipp.fehre@googlemail.com>
2014-07-28 22:55:04 +0100

    getting ready to travis!

---
+++ install_couchbase.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# add the couchbase repo so we can get libcouchbase
+sudo rm -rf /etc/apt/sources.list.d/*
+sudo add-apt-repository -y ppa:pypy/ppa
+wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
+echo "deb http://packages.couchbase.com/ubuntu oneiric oneiric/main" | sudo tee /etc/apt/sources.list.d/couchbase.list
+sudo apt-get update
+
+# install couchbase
+couchbase_file="couchbase-server-enterprise_$COUCHBASE_VERSION_x86_64.deb"
+cd /tmp
+echo "Downloading Couchbase $COUCHBASE_VERSION"
+wget http://packages.couchbase.com.s3.amazonaws.com/releases/$COUCHBASE_VERSION/$couchbase_file
+echo "Installing Couchbase $COUCHBASE_VERSION"
+sudo dpkg -i $couchbase_file
+
+# setting up the server with the test bucket
+CBCLI=/opt/couchbase/bin/couchbase-cli
+$CBCLI node-init -c localhost:8091
+
+$CBCLI cluster-init -c localhost:8091 \
+  --cluster-init-username=Administrator --cluster-init-password=asdasd \
+  --cluster-init-ramsize=256
+
+$CBCLI bucket-create -c localhost:8091 \
+  --bucket=orm_adapter \
+  --bucket-type=couchbase \
+  --bucket-ramsize=256 \
+  --enable-flush=1 \
+  -u Administrator -p asdasd
+
+
+
